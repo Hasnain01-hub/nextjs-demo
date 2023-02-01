0ce670e2d0c85d384e9d09bd22580fee7f7d4ea7
Hasnain Sayyed <70531302+Hasnain01-hub@users.noreply.github.com>
2023-02-01 10:28:59 +0530

    Update [pdetail].js

--- pages/Product/[pdetail].js
+++ pages/Product/[pdetail].js
@@ -76,6 +76,62 @@ const data = [
 ];
 
 export const getStaticProps = (context) => {
+  const data = [
+  {
+    id: 1,
+    name: "Product 1",
+    category: "Category 1",
+    images: [
+      {
+        id: 1,
+        url: "https://www.whoa.in/download/mobile-wallpapers-hd-images-background-wallpapers-5-mobile-wallpaper",
+      },
+    ],
+    description:
+      "Culpa nisi ullamco ex sit consectetur nisi nostrud ullamco eu qui occaecat duis. Cupidatat dolore magna ",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    category: "Category 2",
+    images: [
+      {
+        id: 2,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOJVdoGT_J3FnfDMthTSjmEYIGlQL6z23Nf4VM9YXD99urVeGWU4DOSrnOzvvvsPx-fvI&usqp=CAU",
+      },
+    ],
+    description: "Culpa nisi ullamco ex sit consectetur nisi nostrud",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    category: "Category 3",
+    images: [
+      {
+        id: 3,
+        url: "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      },
+    ],
+    description: "Culpa nisi ullamco ex sit consectetur nisi nostrud",
+    price: 100,
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    category: "Category 4",
+    images: [
+      {
+        id: 4,
+        url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+      },
+    ],
+    description: "Culpa nisi ullamco ex sit consectetur nisi nostrud",
+    price: 100,
+  },
+];
+
   const path = context.params.pdetail;
   const pdata = data.filter((post) => {
     if (post.id.toString() == path) return post;
